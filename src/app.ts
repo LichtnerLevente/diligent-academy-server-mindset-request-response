66268f258976697e03be9874b2667bf85dfe2b47
LichtnerLevente <54024800+LichtnerLevente@users.noreply.github.com>
2024-10-23 14:03:18 +0200

    complete task7

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,9 @@ export default function createApp(options = {}) {
 
 
   type PostBeverageRouteWithStaffRoute = {
+    Headers: {
+      "CodeCool-Beverages-Dietary"?: "vegan" | "lactose-intolerance";
+    }
     Querystring: {
       milk?: "yes" | "no";
       sugar?: "yes" | "no";
@@ -21,33 +24,40 @@ export default function createApp(options = {}) {
     Params: {
       beverage: "coffee" | "tea" | "chai";
     }
-    
+
     Body?: {
       kind: string;
     }
-      
+
   }
 
   app.post<PostBeverageRouteWithStaffRoute>('/api/beverages/:beverage', (request, reply) => {
     const { beverage } = request.params;
-    
+
     const milk = request.query.milk;
     const sugar = request.query.sugar;
     const kind = request.body?.kind;
+    const dietary = request.headers['codecool-beverages-dietary'];
 
     const condiments: string[] = [];
-    if(milk === "yes"){
-      condiments.push("milk")
+    if (milk === "yes") {
+      if (dietary === "lactose-intolerance") {
+        condiments.push("lf-milk")
+      } else if (dietary === "vegan") {
+        condiments.push("oat-milk")
+      } else {
+        condiments.push("milk")
+      }
     }
-    if(sugar === "yes"){
+    if (sugar === "yes") {
       condiments.push("sugar")
     }
-      
 
-    if(kind){
+
+    if (kind) {
       reply.send({ drink: `${kind} ${beverage}`, with: condiments });
     }
-    
+
     reply.send({ drink: beverage, with: condiments });
   })
 
